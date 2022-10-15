78e25c55ad8777f77d2687fa8f2e0d9943880cee
Josegabrielgomezp <Code1801gp@gmail.com>
2022-10-15 00:35:33 +0000

    Random Card Generator done

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+window.onload = function() {
 
-window.onload = function(){
-    console.log('Everything is working');
-}
+    let suits = ["hearts", "spades", "clubs", "diamonds"];
+    let rank = ["A", "K", "Q", "J", 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  
+    function getRandomInt(array) {
+      return Math.floor(Math.random() * array.length);
+    }
+    console.log(suits[getRandomInt(suits)]);
+    console.log(rank[getRandomInt(rank)]);
+    let card = document.querySelector(".card");
+    let rankDiv = document.querySelector(".rank");
+  
+    card.classList.add(suits[getRandomInt(suits)]);
+    rankDiv.innerHTML = rank[getRandomInt(rank)];
+  };
